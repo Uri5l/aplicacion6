246bd2d90c3b3ca2ac48d0d72540dfc1784f97e5
Uri5l <uriel.dsaupoo@gmail.com>
2016-05-02 20:19:03 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -3,8 +3,8 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
-		$('#beep').tap(function (){
-			navigator.notification.beep (1);
+$('#beep').tap(function (){
+navigator.notification.beep (1);
 });//tap beep
 
 $('#vibrar').tap(function (){
